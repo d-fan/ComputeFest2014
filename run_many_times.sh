bb1c7e352d390dd83010da013cb75099763ff431
Aaron Landesman <aaronlandesman@gmail.com>
2014-01-22 11:49:03 -0500

    possibly working random method added to basic

--- run_many_times.sh
+++ run_many_times.sh
@@ -1,7 +1,7 @@
 # arguments: num_times_to_run, first_bot, second_bot, output_name
 
 for ((i = 1; i <= $1; i++)); do
-    ./$2 string_for_bash | tail -1 >> $4 & ./$3 string_for_bash > /dev/null
+    ./$2 randomstring | tail -1 >> $4 & ./$3 randomstring > /dev/null
     echo $i
 done
 grep WIN $4 | wc -l

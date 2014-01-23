3d455a038dab7327908b4a2545761957bb989d2d
Aaron Landesman <aaronlandesman@gmail.com>
2014-01-23 11:30:40 -0500

    2.2 2.4 runs added

--- run_many_times.sh
+++ run_many_times.sh
@@ -1,6 +1,5 @@
 # arguments: num_times_to_run, first_bot, second_bot, output_name
 
-rm $4
 for ((i = 1; i <= $1; i++)); do
     ./$2 anotherstring | tail -1 >> $4 & ./$3 anotherstring > /dev/null
     echo $i
